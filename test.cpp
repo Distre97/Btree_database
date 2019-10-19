277d36455c3e8f881366ead0aa9ba7bafabbd711
Distre97 <yuzhenqi97@foxmail.com>
2019-10-19 13:43:58 +0800

    test file commit

---
+++ test.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+class test{
+
+private:
+    int file_name;
+    
+public:
+    test(int a){
+        this->file_name=a;
+        cout<<this->file_name<<"\n";
+    }
+    ~test(){
+        cout<<s<<" end\n";
+    }
+    void f(int a){
+        this->s=a;
+    }
+
+
+    int s;
+
+};
+
+// class test_p:public test
+// {
+//     test_p(int a){
+        
+//         this->f(a);
+//     }
+//     ~test_p(){
+//         cout<<this->file_name<<"\n";
+//     }
+
+//     void p(int b){
+//         this->s = b;
+//         cout<<this->s<<"\n";
+//     }
+
+// };
+
+int main()
+{
+    test *t = new test(5);
+    t->f(3);
+    delete t;
+    return 0;
+}
+
